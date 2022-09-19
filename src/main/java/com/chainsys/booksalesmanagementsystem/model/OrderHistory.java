854cd2885d49be282418facecbd75ba73f3e278c
Roselie M <rose3001@global.chainsys.com>
2022-09-19 12:08:25 +0530

    project updated

--- src/main/java/com/chainsys/booksalesmanagementsystem/model/OrderHistory.java
+++ src/main/java/com/chainsys/booksalesmanagementsystem/model/OrderHistory.java
@@ -1,76 +1,11 @@
 package com.chainsys.booksalesmanagementsystem.model;
 
-import java.sql.Date;
+public class OrderHistory extends Orders {
 
-public class OrderHistory {
-
-	private int orderId;
-	private String bookId;
-	private String userName;
-	private Date orderDate;
-	private int quantity;
-	private int totalPrice;
-	private String status;
 	private String bookName;
 	private int actualPrice;
 	private byte[] bookImage;
 	private String imagesPath;
-
-	public int getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
-	}
-
-	public String getBookId() {
-		return bookId;
-	}
-
-	public void setBookId(String bookId) {
-		this.bookId = bookId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public Date getOrderDate() {
-		return orderDate;
-	}
-
-	public void setOrderDate(Date orderDate) {
-		this.orderDate = orderDate;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public int getTotalPrice() {
-		return totalPrice;
-	}
-
-	public void setTotalPrice(int totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
 	public String getBookName() {
 		return bookName;
